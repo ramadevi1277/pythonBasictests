86038c70f8ae53a0bea13edee07ddde300d4c870
ramadevi1277 <rongala.ramadevi@gmail.com>
2023-10-02 21:38:32 +0530

    added files to the git

---
+++ pytest_demo/pytest_DOM_prj/pages/BasePage.py
@@ -0,0 +1,33 @@
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+"""
+   This class is the parent of all pages
+   It contains all the generic methods and utilities for all pages
+"""
+
+class BasePage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def do_click(self, by_locator):
+        #WebDriverWait(self.driver, 3).until(EC.visibility_of_element_located(by_locator)).click()
+        self.driver.find_element(By.ID, by_locator).click()
+
+    def do_send_keys(self, by_locator, text):
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator)).send_keys(text)
+
+    def get_element_text(self, by_locator):
+        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
+        return element.text
+
+    def is_visible(self, by_locator):
+        element = WebDriverWait(self.driver,10).until(EC.visibility_of_element_located(by_locator))
+        return bool(element)
+
+    def get_title(self, title):
+        WebDriverWait(self.driver, 10).until(EC.title_is(title))
+        return self.driver.title
