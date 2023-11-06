c28530bfeda48ec003a554bcdbc3427616af3404
ramadevi1277 <rongala.ramadevi@gmail.com>
2023-11-06 12:53:24 +0530

    added files to the git .....

---
+++ Getting_count_from_file.py
@@ -0,0 +1,8 @@
+import requests
+response = requests.get('https://public.karat.io/content/test/test_log.txt')
+count = 0
+ar = response.text.split('\n')
+for i in ar:
+ if 'Block' in i:
+    count = count+1
+print(count)
