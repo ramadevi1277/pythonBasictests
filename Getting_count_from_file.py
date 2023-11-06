f0de2dc335e886601ae282bf1566616a50e3d1d6
ramadevi1277 <rongala.ramadevi@gmail.com>
2023-11-06 13:46:41 +0530

    added files to the git .....

--- Getting_count_from_file.py
+++ Getting_count_from_file.py
@@ -6,3 +6,8 @@ for i in ar:
  if 'Block' in i:
     count = count+1
 print(count)
+
+
+import urllib.request
+import pdb;pdb.set_trace()
+webUrl=urllib.request.urlopen('https://public.karat.io/content/test/test_log.txt')
