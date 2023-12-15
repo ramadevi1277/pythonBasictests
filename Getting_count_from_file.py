b6be27653f8703b94672f3bd1630b92f2e712941
ramadevi1277 <rongala.ramadevi@gmail.com>
2023-12-15 17:07:57 +0530

    addeding examples of request module .....

--- Getting_count_from_file.py
+++ Getting_count_from_file.py
@@ -9,8 +9,19 @@ print(count)
 
 
 import urllib.request
-import pdb;pdb.set_trace()
 webUrl=urllib.request.urlopen('https://public.karat.io/content/test/test_log.txt')
 
 
-fdsds
+def get_books_and_pages_count():
+    response = requests.get("https://public.karat.io/content/test/test_file.txt")
+    list_of_lines= response.text.split('\n')
+    pages_count = 0
+    for line in list_of_lines:
+        #import pdb;pdb.set_trace()
+        pages_count = pages_count + int(line.split(',')[3])
+    print("total_pages", pages_count)
+    print("Total_books", len(list_of_lines))
+    print(list_of_lines)
+
+
+get_books_and_pages_count()
