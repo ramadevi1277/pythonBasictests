4b08a3cd288944465e5568ee682303334175d6d3
ramadevi1277 <rongala.ramadevi@gmail.com>
2023-12-15 17:08:18 +0530

    addeding examples of request module .....

--- reverse_of_words.py
+++ reverse_of_words.py
@@ -1,6 +1,6 @@
 def reverseWords(s):
     str1 = s.split()
-    return " ".join(str1[::-1])
+    print(" ".join(str1[::-1]))
 
 
-print(reverseWords("My name is Ramadevi"))
+reverseWords("My name is Ramadevi")
