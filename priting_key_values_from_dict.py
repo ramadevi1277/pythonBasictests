49d92861f897bd3cb229f035c07f8c6ba8d60543
ramadevi1277 <rongala.ramadevi@gmail.com>
2025-03-01 15:09:56 +0530

    adding comparisn of 2 lists

---
+++ priting_key_values_from_dict.py
@@ -0,0 +1,6 @@
+list1= [{"emp":"john", "dest_no" : "QA Engineer"},{"emp":"Rama", "dest_no": "Software_eng"},{"emp":"devi", "dest_no": "engineer"}]
+
+for each in range(len(list1)):
+    print("emp is %s" %(list1[each]['emp']), end=" and ")
+    print("dest_no is %s"  %(list1[each]['dest_no']))
+
